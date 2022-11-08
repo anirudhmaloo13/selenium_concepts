7ad160f07a7b3d2b1222af6fc3f8bb021b43669e
Anirudh.Maloo <Anirudh.Maloo@BRILAP1668.BRILLIO.COM>
2022-11-08 09:50:14 +0530

    facebook sign up

--- src/com/brillio/basics/LaunchDriver.java
+++ src/com/brillio/basics/LaunchDriver.java
@@ -6,6 +6,7 @@ import java.time.Duration;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 
 public class LaunchDriver {
 
@@ -22,11 +23,28 @@ public class LaunchDriver {
 		String title = driver.getTitle();
 		System.out.println(title);
 		
-		driver.findElement(By.id("email")).sendKeys("Anirudh.maloo@brillio.com");
-		driver.findElement(By.id("pass")).sendKeys("Anirudh123");
-		Thread.sleep(3000);
-		driver.findElement(By.name("login")).click();
-        
-	}
+		driver.findElement(By.linkText("Create New Account")).click();
+		
+	//driver.findElement(By.id("email")).sendKeys("Anirudh.maloo@brillio.com");
+	//	driver.findElement(By.id("pass")).sendKeys("Anirudh123");
+	//	Thread.sleep(3000);
+	//	driver.findElement(By.name("login")).click();
+		
+		driver.findElement(By.name("firstname")).sendKeys("John123");
+		driver.findElement(By.name("lastname")).sendKeys("John123");
+		driver.findElement(By.id("password_step_input")).sendKeys("John123");
+		
+		Select day= new Select(driver.findElement(By.id("day")));
+		day.selectByIndex(14);
+		
+		Select month= new Select(driver.findElement(By.id("month")));
+		month.selectByVisibleText("Dec");
+		
+		Select year= new Select(driver.findElement(By.id("year")));
+		year.selectByValue("2000");
+		
+		driver.findElement(By.xpath("//input[@value='2']")).click();
+		driver.findElement(By.name("websubmit")).click();
+			}
 
 }
